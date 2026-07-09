e04a0aca06fefbfd35d23edb94e527bb62ed087e
JossJL <joss.lazenby@gmail.com>
2026-07-09 16:49:59 +0100

    updated dbt model

--- models/marts/biggest_flop.sql
+++ models/marts/biggest_flop.sql
@@ -1,9 +1,16 @@
-SELECT
+with movies as (
+
+    select * from {{ ref('stg_movies') }}
+
+)
+
+select
     title,
-    mpaa_rating,
+    imdb_rating,
     major_genres,
     worldwide_gross,
     production_budget,
-    (worldwide_gross * 1.0)/production_budget as profit_ratio
-FROM {{ ref('stg_movies') }}
-WHERE production_budget > 100000000
+    (worldwide_gross * 1.0) / production_budget as box_office_multiple
+from movies
+where production_budget > 100000000
+and (worldwide_gross * 1.0) / production_budget < 1
